9b580e5fff37ee1ac972cc23b3944ac178f4ca65
Mischa Dasberg <mischa@dasberg.nl>
2016-01-07 13:19:04 +0100

    get correct process env.

--- test/protractor/config/protractor-jasmine2.travis.conf.js
+++ test/protractor/config/protractor-jasmine2.travis.conf.js
@@ -1,7 +1,7 @@
 var config = require(__dirname + '/protractor-jasmine2.conf').config;
 
-config.sauceUser = process.env.SAUCE_USERNAME;
-config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+config.sauceUser = process.env['SAUCE_USERNAME'];
+config.sauceKey = process.env['SAUCE_ACCESS_KEY'];
 
 config.multiCapabilities.forEach(function (capability) {
     capability['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
