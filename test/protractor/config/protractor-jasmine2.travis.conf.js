14c95ad5d0a91590c871ffd070be0c0a5a083c73
Mischa Dasberg <mischa@dasberg.nl>
2016-01-08 14:19:30 +0100

    update config with correct job number

--- test/protractor/config/protractor-jasmine2.travis.conf.js
+++ test/protractor/config/protractor-jasmine2.travis.conf.js
@@ -1,14 +1,14 @@
 var config = require(__dirname + '/protractor-base.conf').config;
 
 config.framework = 'jasmine2';
-config.sauceUser = 'mdasberg'; //process.env.SAUCE_USERNAME;
-config.sauceKey = '6819c778-bd98-452d-a89f-5ab3902083aa'; //process.env.SAUCE_ACCESS_KEY;
+config.sauceUser = process.env.SAUCE_USERNAME;
+config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 
 config.multiCapabilities =[{
     'browserName': 'chrome',
     'name': 'angular-test-setup - protractor - jasmine2',
-    'tunnel-identifier': '1', //process.env.TRAVIS_JOB_NUMBER,
-    'build': '1', //process.env.TRAVIS_BUILD_NUMBER,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
     'shardTestFiles': true,
     'maxInstances': 10
 }];
